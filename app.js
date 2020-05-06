652e20a26c154f6a29c95361d53ee0e6b4277dd9
Julia Hanlon <jhanlon289@gmail.com>
2020-05-06 16:25:43 -0700

    added main temp and list

--- app.js
+++ app.js
@@ -9,10 +9,21 @@ $(document).ready(function () {
 
     $.ajax({
       type: "GET",
-      url: `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid={4d1769f6552330211a2272f5cf6614ce}`,
+      url: `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=4d1769f6552330211a2272f5cf6614ce`,
       dataType: "json",
     }).then(function (res) {
       console.log(res);
+      $("#city").text(res.city.name);
+      $("#city").append(res.list[0].dt_txt);
+      $("#list-group").append(`<li class= "btn">${res.city.name}</li>`);
+      var kTemp = res.list[0].main.temp;
+      var fTemp = Math.round((kTemp - 273.15) * (9 / 5) + 32);
+      $("#temp").text(`"Temperature ${fTemp} F"`);
+      $("#humidity").text(res.list[0].main.humidity);
+      $("#windSpeed").text(res.list[0].wind.speed);
+
+      // $("#uvIndex").text(res.Rated);
+      $();
     });
   });
 });
