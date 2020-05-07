1fb7f1aee6abb5742016728a7ae5a4bc14dab55a
Julia Hanlon <jhanlon289@gmail.com>
2020-05-07 10:46:44 -0700

    formatting

--- app.js
+++ app.js
@@ -16,14 +16,36 @@ $(document).ready(function () {
       $("#city").text(res.city.name);
       var today = res.list[0].dt_txt;
       console.log(today);
-      $("#list-group").append(`<li class= "btn">${res.city.name}</li>`);
+      $("#list-group").append(`<li class= "btn cities">${res.city.name}</li>`);
       var kTemp = res.list[0].main.temp;
       var fTemp = Math.round((kTemp - 273.15) * (9 / 5) + 32);
-      $("#temp").text(`"Temperature ${fTemp} F"`);
-      $("#humidity").text(res.list[0].main.humidity);
-      $("#windSpeed").text(res.list[0].wind.speed);
+      $("#temp").text(`Temperature ${fTemp} F`);
+      $("#humidity").text("Humidity" + " " + res.list[0].main.humidity);
+      $("#windSpeed").text("Wind Speed" + " " + res.list[0].wind.speed + "mph");
 
       // $("#uvIndex").text(res.Rated);
+
+      // for 5 day forecast
+      // var tomorrow = res.
+      var kTemp2 = res.list[8].main.temp;
+      var fTemp2 = Math.round((kTemp2 - 273.15) * (9 / 5) + 32);
+      $("#tomorrow").text(`Temperature ${fTemp2} F`);
+
+      var kTemp3 = res.list[16].main.temp;
+      var fTemp3 = Math.round((kTemp3 - 273.15) * (9 / 5) + 32);
+      $("#dayAfter").text(`Temperature ${fTemp3} F`);
+
+      var kTemp4 = res.list[24].main.temp;
+      var fTemp4 = Math.round((kTemp4 - 273.15) * (9 / 5) + 32);
+      $("#day3").text(`Temperature ${fTemp4} F`);
+
+      var kTemp5 = res.list[32].main.temp;
+      var fTemp5 = Math.round((kTemp5 - 273.15) * (9 / 5) + 32);
+      $("#day4").text(`Temperature ${fTemp5} F`);
+
+      var kTemp6 = res.list[40].main.temp;
+      var fTemp6 = Math.round((kTemp6 - 273.15) * (9 / 5) + 32);
+      $("#dayAfter").text(`Temperature ${fTemp6} F`);
     });
   });
   $(document).on("click", ".btn", function () {
